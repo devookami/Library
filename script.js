37db004463f7f57de851395c1bb1ccf6ab79a3e2
wolf <devcevu@gmail.com>
2024-06-23 18:21:16 +0200

    add grid and make DOM cards

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const toggleAdd = document.querySelector("#toggleaAdd");
 const card = document.querySelector(".card");
 const form = document.querySelector("#form");
+const change = document.querySelector("#change");
 
 const myLibrary = [];
 
@@ -13,27 +14,29 @@ class Book {
     }
 }
 
-function addBookToLibrary() {
-    myLibrary.push(newBook);
-}
-
 toggleAdd.addEventListener("click", () => {
     toggle();
 });
 
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    // createBookCard();
+    addBookToLibrary();
+    toggle();
+    event.target.reset();
+});
+
 function toggle() {
     card.classList.toggle("toggle");
-    if(!toggleAdd.disabled == true){
+    if (!toggleAdd.disabled == true) {
         toggleAdd.disabled = false;
     } else {
         toggleAdd.disabled = true;
     }
 }
 
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    let title = document.querySelector("#title").value;
+function addBookToLibrary() {
+    let  title = document.querySelector("#title").value;
     let author = document.querySelector("#author").value;
     let pages = document.querySelector("#pages").value;
     let isRead = document.querySelector("#isRead");
@@ -43,8 +46,24 @@ form.addEventListener("submit", (event) => {
         isRead = true;
     }
     let newBook = new Book(title, author, pages, isRead);
-
+    createBookCard(newBook);
     myLibrary.push(newBook);
-    toggle();
-    event.target.reset();
-});
+}
+
+function createBookCard(name) {
+    let bookCard = document.createElement('div');
+    bookCard.style.border = '1px solid #ccc';
+    bookCard.style.padding = '10px';
+    bookCard.style.borderRadius = '5px';
+    bookCard.style.boxShadow = '0 0 5px rgba(0,0,0,0.1)';
+
+    bookCard.innerHTML = `
+        <h3>Naziv: ${name.title}</h3>
+        <p>Autor: ${name.author}</p>
+        <p>Strana: ${name.pages}</p>
+        <button id="change">${name.isRead ? "Procitano" : "Nije Procitano"}</button>
+        <button>remove</button>
+    `;
+
+    document.querySelector('#book-grid').appendChild(bookCard);
+}
