927505d81e31a187eccb35969e17a81cf428ef49
wolf <devcevu@gmail.com>
2024-06-23 14:07:26 +0200

    constructor and pushing to the array

--- script.js
+++ script.js
@@ -1,20 +1,50 @@
 const toggleAdd = document.querySelector("#toggleaAdd");
 const card = document.querySelector(".card");
-
-toggleAdd.addEventListener("click", () => {
-    card.classList.toggle('toggle');
-    toggleAdd.disabled = true;
-});
+const form = document.querySelector("#form");
 
 const myLibrary = [];
 
-function Book(title, author, pages, isRead) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.isRead = isRead;
+class Book {
+    constructor(title, author, pages, isRead) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.isRead = isRead;
+    }
 }
 
 function addBookToLibrary() {
-    // do stuff here
+    myLibrary.push(newBook);
 }
+
+toggleAdd.addEventListener("click", () => {
+    toggle();
+});
+
+function toggle() {
+    card.classList.toggle("toggle");
+    if(!toggleAdd.disabled == true){
+        toggleAdd.disabled = false;
+    } else {
+        toggleAdd.disabled = true;
+    }
+}
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let title = document.querySelector("#title").value;
+    let author = document.querySelector("#author").value;
+    let pages = document.querySelector("#pages").value;
+    let isRead = document.querySelector("#isRead");
+    if (!isRead.checked) {
+        isRead = false;
+    } else {
+        isRead = true;
+    }
+    let newBook = new Book(title, author, pages, isRead);
+
+    myLibrary.push(newBook);
+    toggle();
+    event.target.reset();
+});
